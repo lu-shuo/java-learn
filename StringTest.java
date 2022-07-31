a9845c0c566839cb7f620d3d77fd06ec6ab5e3bc
lushuo <lushuo_code@163.com>
2022-07-31 20:16:09 +0800

    String

---
+++ StringTest.java
@@ -0,0 +1,20 @@
+public class StringTest {
+  public static void main(String[] args) {
+    int no = 10;
+    String str = "abcdef";
+    String str1 = str + "xyz" + no;
+
+    System.out.println(str1);
+
+    double pi = 3.1415926;
+
+    System.out.println(str + pi);
+
+    boolean b = false;
+
+    System.out.println(str + b);
+
+    // System.out.println(pi + b);
+
+  }
+}
